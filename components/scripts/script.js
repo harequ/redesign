94c261607458803196d23387a9fc3445fa831ee9
harequ <harequ@gmail.com>
2015-06-25 03:23:27 +0300

    Fixed sticky navigation, added highlighting for navigation items while scrolling, fixed nav menu when resize the window

--- components/scripts/script.js
+++ components/scripts/script.js
@@ -1,5 +1,115 @@
 $(function() {
-	
+
+	var fadeStart = 0,
+		fadeUntil = 400,
+		appearStart = 400,
+		hero = $('.hero'),
+		branches = $('.branches'),
+		snow = $('.snow'),
+		threeCircles = $('#intro dl'),
+		nav = $('#nav'),
+		navOffset = $('#nav').offset().top,
+		navHeight = nav.outerHeight(),
+		pull = $('#pull'),
+		menu = $('#nav .navbar'),
+		menuHeight = menu.height();
+
+	// Smoth scrolling
+	$(".scroll").click(function(event) {
+	    event.preventDefault();
+	    $('html, body').animate({ scrollTop : $(this.hash).offset().top - navHeight +1 } , 700);
+    });
+
+	// Scrolling events
+	$(window).scroll(function() {
+		var offset = $(this).scrollTop(),
+			heightOfIntro = $('header').height(),
+	    	opacity = 0,
+	    	opacity2 = 1;
+
+	    // Parallax effect for branches and snow
+		if(offset <= heightOfIntro) {
+			branches.css({ 'transform' : 'translate(0px, '+ offset / 3.8 +'%)' });
+			snow.css({ 'transform' : 'translate(0px, ' + offset * 1.2 +'px)' });
+		}
+
+		// Fade in and Fade out effect for herotext and threeCircles
+	    if(offset <= fadeStart) {
+	        opacity = 1;
+	        opacity2 = 0;
+	    } else if( offset <= fadeUntil ){
+	        opacity = 1 - offset/fadeUntil;
+	        opacity2 = offset/fadeUntil;
+	    }
+
+	    hero.css('opacity', opacity);
+	    threeCircles.css('opacity', opacity2);
+
+	    // Highlighting active nav menu items
+	    var windowPos = offset + navHeight;
+
+	    $('.navbar li a').removeClass('active');
+
+	    if(windowPos > $('#work').offset().top) {
+		    $('.navbar li a').removeClass('active');
+		    $('a[href$="#work"]').addClass('active');
+	    }
+
+	    if(windowPos > $('#design').offset().top) {
+		    $('.navbar li a').removeClass('active');
+		    $('a[href$="#design"]').addClass('active');
+	    }
+
+	    if(windowPos > $('#skills').offset().top) {
+		    $('.navbar li a').removeClass('active');
+		    $('a[href$="#skills"]').addClass('active');
+	    }
+
+	    if(windowPos > $('#contact').offset().top) {
+		    $('.navbar li a').removeClass('active');
+		    $('a[href$="#contact"]').addClass('active');
+	    }
+	});
+
+	// When clicking on Menu button, it toggles the menu
+	$(pull).on('click', function(e) {
+		e.preventDefault();
+		menu.slideToggle();
+	});
+
+	// When menu is opened (in mobile version), slideup the menu when clicking anywhere outside
+	$(document).on('click', function(event) {
+		if (!$(event.target).closest('#pull').length && pull.is(':visible') && menu.is(':visible')) {
+			menu.slideUp();
+		}
+	});
+
+    // Remove style attribute (display:block; or display:none;) when resize the window
+	$(window).resize(function() {
+		menu.removeAttr('style');	
+	});
+
+	/*
+	=============================================
+	=== Carousel
+	=============================================
+	*/
+
+	$('.carousel').slick({
+		dots: true,
+		autoplay: true,
+  		autoplaySpeed: 5000
+	});
+
+	/*
+	=============================================
+	=== Sticky nav 
+	=============================================
+	*/
+	var sticky = new Waypoint.Sticky({
+	  element: $(nav)
+	});
+
 	/*
 	=============================================
 	=== IE Fix for "jumpy" fixed background
@@ -15,106 +125,5 @@ $(function() {
             window.scrollTo(0, currentScrollPosition - wheelDelta);
         });
 	}
-	/*
-	=============================================
-	=== Intro page
-	=============================================
-	*/
 
-	var fadeStart = 0, // 100px scroll or less will equiv to 1 opacity
-		fadeUntil = 400, // 400px scroll or more will equiv to 0 opacity
-		appearStart = 400, // 400px scroll or more will equiv to 0 opacity
-		hero = $('.hero'),
-		branches = $('.branches'),
-		snow = $('.snow'),
-		threeCircles = $('#intro dl'),
-		nav = $('#nav'),
-		navOffset = $('#nav').offset().top,
-		navHeight = nav.outerHeight();
-		nav.wrap('<div class="nav-placeholder"></div>');
-
-	$(".scroll").click(function(event) {
-	    event.preventDefault();
-	    $('html, body').animate({ scrollTop : $(this.hash).offset().top - navHeight +1 } , 700);
-    });
-
-	$(window).scroll(function() {
-		var offset = $(this).scrollTop();
-		var heightOfIntro = $('header').height();
-
-		if(offset >= navOffset) {
-			nav.addClass('fixed');
-			$('.nav-placeholder').css('height', navHeight);
-		} else {
-			nav.removeClass('fixed');
-			$('.nav-placeholder').css('height', '');
-		}
-
-		if(offset <= heightOfIntro) {
-			branches.css({ 'transform' : 'translate(0px, '+ offset / 3.8 +'%)' });
-			snow.css({ 'transform' : 'translate(0px, ' + offset * 1.2 +'px)' });
-		}
-
-	    var opacity = 0;
-	    var opacity2 = 1;
-
-	    if(offset <= fadeStart) {
-	        opacity = 1;
-	        opacity2 = 0;
-	    } else if( offset <= fadeUntil ){
-	        opacity = 1 - offset/fadeUntil;
-	        opacity2 = offset/fadeUntil;
-	    }
-
-
-	    hero.css('opacity', opacity);
-	    threeCircles.css('opacity', opacity2);
-	});
-
-	/*
-	=============================================
-	=== Sticky navigation menu
-	=============================================
-	*/
-
-	var pull = $('#pull'),
-		menu = $('#nav .navbar'),
-		menuHeight = menu.height(),
-		width = $(window).width();
-
-	// When clicking on Menu button, it toggles the menu
-	$(pull).on('click', function(e) {
-		e.preventDefault();
-		menu.slideToggle();
-	});
-
-	// When menu is opened (in mobile version), slideup the menu when clicking anywhere outside
-	$(document).on('click', function(event) {
-	  if (!$(event.target).closest('#pull').length && pull.is(':visible') && menu.is(':visible')) {
-		menu.slideUp();
-	  }
-	});
-
-    // Show the menu if it was hidden when resizing to > 650 
-	$(window).resize(function() {
-		navOffset = $('#nav').offset().top;
-
-		if(width > 650 && menu.is(':hidden')) {
-			menu.removeAttr('style');
-		} else if (width < 650 && menu.not(':hidden')) {
-			menu.attr('style', ':hidden');
-		}
-	});
-
-	/*
-	=============================================
-	=== Carousel
-	=============================================
-	*/
-
-	$('.carousel').slick({
-		dots: true,
-		autoplay: true,
-  		autoplaySpeed: 5000
-	});
 });
